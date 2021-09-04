bbe1a93d6baeb6a1a606bdb760fa25917ddbce33
lch-howger <sharpe_nft_dev@163.com>
2021-09-04 15:26:07 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -66,6 +66,13 @@ module.exports = {
             // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
             // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         },
+
+        rinkeby: {
+            provider: () => new HDWalletProvider(mnemonic, `wss://rinkeby.infura.io/ws/v3/` + infura_project_id),
+            network_id: 4,
+            gas: 5500000,
+        },
+
         // Useful for private networks
         // private: {
         // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),

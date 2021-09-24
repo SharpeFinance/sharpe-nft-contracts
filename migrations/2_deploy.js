50b7d06adc60db518a4cb5d4316d4fa43227781c
lch-howger <sharpe_nft_dev@163.com>
2021-09-24 23:23:45 +0800

    Update 2_deploy.js

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -4,8 +4,7 @@ module.exports = function (deployer) {
     //deployer.deploy(SharpeFinanceCattle, "ipfs://Qma4zuWaK11J1qcChzWMSmz3pfYYFu9dxg3eh4T5E3LVeJ/",1,1);
     deployer.deploy(
         SharpeFinanceCattle,
-        "0x9b0B0076B9e6caFc8f0b9cEED9A5190c0fCa91ea",
-        "ipfs://QmdCURxxBqu54FkUHaZrX4ySRhSScvouaamhg7mWPmAcj7/",
+        "ipfs://Qmeta9UkEgx42G4P2CtFNfBdmEooA7ioaxw6DXy9vKKg5A/",
         1,
         1
     );
